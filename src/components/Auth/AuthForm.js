4c6dac62a890bb3fe294aced48b88572fbdd5d54
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-08-18 14:25:19 -0400

    functionality added for sign in

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -21,35 +22,55 @@ const AuthForm = () => {
     // optional: we can add validation here.
     // console.log(enteredEmail, enteredPassword)
 
+    setIsLoading(true);
+    let url;
     if (isLogin) { // If user is logged in 
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAUpvqcwLjG2JhxIALjxfIWEM3LXAztE1Q';
     }
     else { // If user is in sign up
       // sending sign up request:
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAUpvqcwLjG2JhxIALjxfIWEM3LXAztE1Q',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-        // as we know fetch returns a 'promise' hence we can add 'catch' here to handle errors or 'then' for regular responses
-      ).then(response => {
-        if (response.ok) {
-
-        } else {
-          // if it fails, we will throw an error. so if it does fail, it means the response data we get back will hold some extra information. so we will look into response data by calling response.json() and it also returns promise so we can call 'then' here to get access to the actual response data and can show for example error modal. 
-          return response.json().then(data => {
-            console.log(data);
-          });
-
-        }
-      });
+      // we could use 'async/await' way. Here we are using promises with 'then', 'catch'.
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAUpvqcwLjG2JhxIALjxfIWEM3LXAztE1Q';
     }
+    fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      // as we know fetch returns a 'promise' hence we can add 'catch' here to handle errors or 'then' for regular responses
+    ).then(response => {
+      setIsLoading(false);
+      if (response.ok) {
+        // if response is ok
+        return response.json();
+      } else {
+        // if it fails, we will throw an error. so if it does fail, it means the response data we get back will hold some extra information. so we will look into response data by calling response.json() and it also returns promise so we can call 'then' here to get access to the actual response data and can show for example error modal. 
+        return response.json().then(data => {
+          // show an error message
+          // console.log(data);
+          let errorMessage = 'Authentication failed!';
+          // if(data && data.error && data.error.message){
+          //   errorMessage = data.error.message;
+          // }
+          throw new Error(errorMessage);
+        });
+
+      }
+      // To get response data in success case and catch for handling errorr.
+    }).then((data) => {
+      // if no error
+      console.log(data);
+    }).catch(err => {
+      // if error
+      alert(err.message);
+    });
 
   };
 
@@ -74,7 +95,8 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request...</p>}
           <button
             type='button'
             className={classes.toggle}
