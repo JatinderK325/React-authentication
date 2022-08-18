73324b4dcbc6c60520c8f941628861727c06aaf5
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-08-18 14:58:24 -0400

    auth-context used

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,8 +1,10 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const authCtx = useContext(AuthContext);
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -66,7 +68,8 @@ const AuthForm = () => {
       // To get response data in success case and catch for handling errorr.
     }).then((data) => {
       // if no error
-      console.log(data);
+      // console.log(data);
+      authCtx.login(data.idToken);
     }).catch(err => {
       // if error
       alert(err.message);
